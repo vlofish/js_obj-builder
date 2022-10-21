b263718c64599a5c9563611af580aa6351bee992
VMijael <11095094+vmijael@users.noreply.github.com>
2022-10-21 16:26:31 -0500

    Re-Structure of Files

--- src/app/main.js
+++ src/app/main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
+import javascriptLogo from '/javascript.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
